9e3605f897eb787b50775787aa4b28a652d82df2
Richard <richardnjoku427@gmail.com>
2025-05-29 10:56:12 +0300

    Update meta information

--- src/components/pages/home/banner/index.tsx
+++ src/components/pages/home/banner/index.tsx
@@ -7,12 +7,12 @@ export default function Banner() {
     return (
         <section className={styles.banner}>
             <Stack className={styles.banner__stack} align="center">
-                <Reveal>
+                <Reveal delay={250}>
                     <Title fz={50} ta="center" c="white" order={1}>
                         Elevate Leadership. Empower Change. Begin Your Journey Today.
                     </Title>
                 </Reveal>
-                <Reveal delay={200}>
+                <Reveal delay={300}>
                     <Text maw={580} c="white" ta="center"> 
                         The Center for Executive Education at USIU-Africa offers forward-thinking programs
                         designed for professionals seeking to lead with purpose, adapt with agility, and drive
