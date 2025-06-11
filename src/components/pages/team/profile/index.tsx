10616b7f84328b4810970195cb4548c88e959213
Richard <richardnjoku427@gmail.com>
2025-06-11 10:14:23 +0300

    text justification

--- src/components/pages/team/profile/index.tsx
+++ src/components/pages/team/profile/index.tsx
@@ -3,6 +3,7 @@ import { Grid, Text, Title, Avatar, Badge, Stack, Divider } from "@mantine/core"
 
 import styles from "./profile.module.scss";
 
+import SlideIn from "@/components/shared/io/slide-in";
 import { teamsData } from "@/components/pages/course/teams";
 
 export default function Profile() {
@@ -15,24 +16,33 @@ export default function Profile() {
         <section className={styles.profile}>
             <Grid justify="center">
                 <Grid.Col span={{base: 12, md: 4, lg: 3}}>
-                    <Avatar size={250} src={user.image} />
+                    <SlideIn direction="left">
+                        <div className={styles.profile__user}>
+                            <Avatar size={220} src={user.image} />
+                        </div>
+                    </SlideIn>
                 </Grid.Col>
+
                 <Grid.Col span={{base: 12, md: 6, lg: 6}}>
-                    <Stack>
-                        <Title size="h2">{user.name}</Title>
-                        <Badge 
-                            radius="xs" 
-                            ta="center" 
-                            variant="light"
-                            fz={10} 
-                        >
-                            {user.role}
-                        </Badge>
-                        <Divider />
-                        {user.description.map(el => (
-                            <Text c="gray.7">{el}</Text>
-                        ))}
-                    </Stack>
+                    <SlideIn direction="right">
+                        <Stack>
+                            <Title size="h2">{user.name}</Title>
+                            <Badge 
+                                radius="xs" 
+                                ta="center" 
+                                variant="light"
+                                fz={10} 
+                            >
+                                {user.role}
+                            </Badge>
+                            <Divider />
+                            {user.description.map(el => (
+                                <Text c="gray.7" fz={14} ta="justify">
+                                    {el}
+                                </Text>
+                            ))}
+                        </Stack>
+                    </SlideIn>
                 </Grid.Col>
             </Grid>
         </section>
