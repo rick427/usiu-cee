f4adebf2965f37edd8a4341fa32b7186690fa473
Richard <richardnjoku427@gmail.com>
2025-06-18 10:01:11 +0300

    Added sitemap generator

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import path from 'node:path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import Sitemap from 'vite-plugin-sitemap';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://usiu-cee.netlify.app',
+      exclude: ['/admin', '/private'],  // exclude admin or other private routes
+      generateRobotsTxt: true, // generate both sitemap.xml and robots.txt
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
